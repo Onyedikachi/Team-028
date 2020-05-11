4743fde2ab7a6574f993b43ef4964e7daed7aa9d
Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-11 17:58:57 +0100

    feat(controllers): add user routes

--- src/backend/router/index.js
+++ src/backend/router/index.js
@@ -1,5 +1,10 @@
-const express = require('express');
+import express from 'express';
+
+import userRoutes from '../controllers/Users/routes';
+
 
 const router = express.Router();
+
+router.use('/user', userRoutes);
 
 module.exports = router;
