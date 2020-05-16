bddc42fd3055a8d7bd288292b830e47def5797d1
Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-16 10:42:20 +0100

    feat : update relationships between the models

--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -5,6 +5,9 @@ import UserModel from './UserModel';
 import AuditModel from './AuditModel';
 import SessionModel from './SessionModel';
 import OrganizationModel from './OrganizationModel';
+import RoleModel from './RoleModel';
+import PrivilegeModel from './PrivilegeModel';
+import RolePrivilegeModel from './RolePrivilegeModel';
 
 
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
@@ -28,9 +31,17 @@ const User = UserModel(sequelize, Sequelize);
 const Audit = AuditModel(sequelize, Sequelize);
 const Session = SessionModel(sequelize, Sequelize);
 const Organization = OrganizationModel(sequelize, Sequelize);
+const Role = RoleModel(sequelize, Sequelize);
+const Privilege = PrivilegeModel(sequelize, Sequelize);
+const RolePrivilege = RolePrivilegeModel(sequelize, Sequelize);
 
-User.belongsTo(Organization, { foreignKey: 'organizationId' });
-Organization.hasMany(User);
+User.belongsTo(Organization, { foreignKey: 'organizationId', as: 'organizations' });
+Organization.hasMany(User, { as: 'users' });
+
+Role.belongsToMany(Privilege, { through: 'roleprivileges', foreignKey: 'roleId', as: 'privileges' });
+RolePrivilege.belongsTo(Role, { foreignKey: 'roleId' });
+RolePrivilege.belongsTo(Privilege, { foreignKey: 'privilegeId' });
+Privilege.belongsToMany(Role, { through: 'roleprivileges', foreignKey: 'privilegeId', as: 'roles' });
 
 sequelize.sync({ force: true }).then(() => {
   logger.info('Database & tables created here');
@@ -41,5 +52,8 @@ module.exports = {
   User,
   Audit,
   Session,
-  Organization
+  Organization,
+  Role,
+  Privilege,
+  RolePrivilege
 };
