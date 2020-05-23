860283b556034501793b76e48fa5ccc3280b9341
Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-23 21:30:59 +0100

    feat: allow only users with create projectCategory privilege

--- src/backend/controllers/ProjectCategory/index.js
+++ src/backend/controllers/ProjectCategory/index.js
@@ -9,6 +9,13 @@ import * as Model from '../../models';
 module.exports.create = async (req, res) => {
   const { name, createdBy, description } = req.body;
 
+  // check if creator has the privileges
+  const userRoles = await Model.Role.findByPk(createdBy, { include: ['privileges'] });
+  const userPrivileges = userRoles.get({ plain: true }).privileges;
+
+  const privilege = userPrivileges.filter((element) => element.privilegeId === 6);
+  if (!privilege || privilege.length < 1) return res.status(400).json({ status: 'error', message: 'you don\'t have this privilege' });
+
   try {
     await Model.ProjectCategory.create({
       name,
